3841662096267be053d546cc887183e5a38520a4
DorianWojcik <Dorian.Wojcik@gmail.com>
2020-01-09 12:53:23 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        String alpha = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ.,!?";
+        String input = "TEXT";
+        StringBuilder output = new StringBuilder();
+        char c;
+        int n;
+        int offset = 1;
+
+        for (int i = 0; i < input.length(); i++) {
+            c = input.charAt(i);
+            n = alpha.indexOf(c) + offset;
+            output.append(alpha.charAt(n));
+        }
+        System.out.println(output);
+
+
+    }
+}
