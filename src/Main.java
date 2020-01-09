175816dbe8054b73b3383d8538e2ce373085ca3d
DorianWojcik <Dorian.Wojcik@gmail.com>
2020-01-09 12:59:50 +0100

    add some stuff

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
     public static void main(String[] args) {
 
         String alpha = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ.,!?";
-        String input = "TEXT";
+        String input = "TEXT?";
         StringBuilder output = new StringBuilder();
         char c;
         int n;
@@ -11,7 +11,7 @@ public class Main {
 
         for (int i = 0; i < input.length(); i++) {
             c = input.charAt(i);
-            n = alpha.indexOf(c) + offset;
+            n = (alpha.indexOf(c) + offset) % alpha.length();
             output.append(alpha.charAt(n));
         }
         System.out.println(output);
